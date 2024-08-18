f43781bb424c07395f819aed5fcda083d49ad0bf
diy0r <sabirovdiyorkind@gmail.com>
2024-08-18 16:43:45 +0500

    chore: lint commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,10 +4,24 @@ export default {
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'refact', 'chore', 'doc', 'test'],
+      [
+        'feat',
+        'fix',
+        'refactor',
+        'chore',
+        'doc',
+        'test',
+        'style',
+        'perf',
+        'revert',
+      ],
     ],
-    'scope-enum': [2, 'always', ['lib', 'test', 'chore']],
+
+    'scope-enum': [2, 'always', ['lib', 'test']],
     'scope-empty': [0, 'never'],
+    'header-max-length': [2, 'always', 70],
+    'body-max-line-length': [0, 'never'],
+    'body-leading-blank': [0, 'never'],
     'subject-case': [2, 'never', ['start-case', 'pascal-case', 'upper-case']],
   },
 };
